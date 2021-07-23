1633d135bbca66a946a86d6c3cf815ee13263c37
Grievin <grievin.okumu@student.moringaschool.com>
2021-07-23 18:03:25 +0300

    add jquery toggle  methods on what we do section

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-    alert("ready")
+
+    $("#design").on("click", function(){
+        
+        $("#pdesign").toggle(1000);
+        
+        $("#des").css("font-weight", "bold");
+
+        $("#design").toggle(1000);
+        
+    });
+
+    $("#pdesign").on("click", function(){
+
+        $("#design").show(1000);
+
+        $("#des").css("font-weight", "normal");
+
+        $("#pdesign").hide(1000)
+
+    });
+
+    $("#development").on("click", function(){
+
+        $("#pdevelopment").toggle(1000);
+
+    });
+    $("#product").on("click", function(){
+
+        $("#pproduct").toggle(1000);
+
+    });
 });
